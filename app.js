ff389516ddeb49f88323ccc8411d05b3d41d59bd
Feri Hermawan <ferihermawan007@gmail.com>
2023-04-29 08:21:25 +0700

    menambahkan CRUD

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
 /* Module */
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const app = express();
-const bodyParser = require('body-parser')
-const moment = require('moment');
+const bodyParser = require("body-parser");
+const moment = require("moment");
 
 /* Set view engine ke EJS */
-app.set('views', path.join(__dirname, 'src/views')); //set lokasi views ejs
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "src/views")); //set lokasi views ejs
+app.set("view engine", "ejs");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
 /* Koneksi ke Database */
-require('./src/database/db.js');
+require("./src/database/db.js");
 
-const userModel = require('./src/database/model/userModel');
-const todoListModel = require('./src/database/model/taskModel.js');
+const userModel = require("./src/database/model/userModel");
+const todoListModel = require("./src/database/model/taskModel.js");
 
 /*Routing */
 
-app.get('/tas-view',(req,res)=>{
-    const user = userModel.find().then((hasil)=>{
+//Melihat Data
+app.get("/task-view",(req,res)=>{
+    const user = todoListModel.find().then((hasil)=>{
         res.send(hasil);
     });
 
 });
 
-app.post('/tambah-data',(req, res)=>{
+//Menambah data
+app.post("/task-add",(req, res)=>{
 
     const dataInputPost = req.body;
-
     const task = new todoListModel({
         judul: dataInputPost.judul,
         deskripsi: dataInputPost.deskripsi,
@@ -48,12 +49,63 @@ app.post('/tambah-data',(req, res)=>{
     // });
     })
 
+//Mencari data berdasarkan idObjek
+app.get("/task-find/:objectId", (req, res)=>{
+
+    const objId = req.params.objectId;
+    todoListModel.find({"_id": objId})
+    .then((result)=>{
+        res.send(result);
+    })
+    .catch((e)=>
+    {
+        res.status(404).json(
+            {
+                message: "Data tidak ditemukan"
+            } 
+        )
+    });
+});
+
+//Mengubah Status Data
+app.get("/task-completed/:objId",(req, res)=>{
+
+    const objId = req.params.objId;
+    const modelTask = todoListModel.updateOne({_id : objId}, {$set: {keterangan: "selesai"}});
+
+    modelTask.then(()=>{
+        res.redirect(301,'/');
+    })
+    .catch((err)=>{
+        res.status(400).json(
+            {
+                message: "Bad Request"
+            });
+    });
+})
+
+//Menghapus Data
+app.get('/task-delete/:objId',(req, res)=>{
+
+    const objId = req.params.objId;
+    const modelTask = todoListModel.deleteOne({_id: objId})
+    .then(()=>
+    {
+        res.send("Data berhasil di hapus");
+    })
+    .catch((err)=>
+    {
+        res.status(301).json({ message: "Bad Request" });
+    });
+});
+
+//Halaman Home
 app.get('/',(req,res)=> {
     const dataTask = todoListModel.find();
 
     dataTask.then((result)=>{
         console.log(result.title);
-        res.render('index',{
+        res.render("index",{
             dataTask: result
         });
     });
